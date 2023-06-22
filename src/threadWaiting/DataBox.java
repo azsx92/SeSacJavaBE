88f8e6c258914bbcc5d1abf8c80fbaa0c4035dfe
azsx92 <azsx92@gmail.com>
2023-06-23 02:06:21 +0900

    thread waiting

--- src/threadWaiting/DataBox.java
+++ src/threadWaiting/DataBox.java
@@ -1,14 +1,34 @@
 package threadWaiting;
 
 public class DataBox {
+//    Step 1. 쓰기 쓰레드 동작 (데이터출력)
+//    Step 2. 읽기 쓰레드 깨우기 (notify())
+//    Step 3. 쓰기 쓰레드 일시정지 (wait())
+//    Step 4. 읽기 쓰레드 동작 (데이터읽기)
+//    Step 5. 쓰기 쓰레드 깨우기 (notify())
+//    Step 6. 읽기 쓰레드 일시정지 (wait())
+//    Step 1~6. 반복
+    boolean isEmpty = true;
     int data;
+
     public synchronized void inputData(int data) throws InterruptedException {
+        if (!isEmpty) {
+            wait();
+        }
+        isEmpty = false;
+
         this.data = data;
         System.out.println("입력 데이터: " + data);
+        notify();
     }
+    public synchronized void outputData() throws InterruptedException {
+        if (isEmpty) {
+            wait();
+        }
+        isEmpty = true;
 
-    public synchronized void outputData()
-            throws InterruptedException {
+        this.data = data;
         System.out.println("출력 데이터: " + data);
+        notify();
     }
 }
